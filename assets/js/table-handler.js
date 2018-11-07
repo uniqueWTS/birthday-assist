d0d602dfc0c989e625d9f2d28c388d2c8472c541
uniqueWTS <710029721@qq.com>
2018-11-07 12:21:19 +0800

    powered by jekyll

--- assets/js/table-handler.js
+++ assets/js/table-handler.js
@@ -3,10 +3,12 @@
 	var db = 'birthAssist';
 
 	// Alert
-	var ms_showAlert = 2000;
-	
+	var ms_showAlert = 1000;
 
 	$('document').ready(function () {
+		// 弹框隐藏
+		$('.alert').hide();
+		showAll();
 	/*
 	1.文档加载完毕，显示全部（人物记录）
 	2.点击新建按钮，新建（人物记录）
@@ -15,31 +17,26 @@
 	5.点击删除按钮，删除（人物记录）
 	*/
 	// 1
-	showAll(); 
-	$('.alert').hide();
+	
 	$('.dropdown-menu').on('click',function (event) {
 		switch(event.target.innerHTML){
-			case '显示全部': showAll(); break;
-			case '新建': break; // bootstrap show modal
-			case '查询': break; // bootstrap show modal
-			case '更改': break; // bootstrap show modal
-			case '删除': break; // bootstrap show modal
-			default: break;
+			case '显示全部': 
+				showAll(); break;
+			case '新建': 
+				$('#btnNewSave').on('click', newAndSave);　break; // bootstrap show modal
+			case '查询': 
+				$('#btnQuery').on('click', queryRecord);　break; // bootstrap show modal
+			case '更改': 
+				$('#btnUpdate').on('click', updateRecord);　break; // bootstrap show modal
+			case '删除': 
+				$('#btnRemove').on('click', removeRecord);　break; // bootstrap show modal
+			default: alert(event.target.innerHTML + " needs a handler!"); break;
 		}
 	});
-	// 2
-	$('#btnNewSave').on('click', newAndSave);
-
-	// 3
-	$('#btnQuery').on('click', queryRecord);
+	
 });
 
-	// 4
-	$('#btnUpdate').on('click', updateRecord);
-
-	// 5
-	$('#btnRemove').on('click', removeRecord);
-
+	
 /*
 显示全部，新建，查询，修改和删除
 */
@@ -48,13 +45,13 @@ function removeRecord(){
 	var oldName = $('#nameBeforeRemove').val(),
 	success = isRemove(oldName);
 	$('#removeRecordModal').modal('hide');
-	showAll();
 	if (success) {
 			writeAlert("删除成功！", 'alert-success');
 		} else {
 			writeAlert("删除失败", 'alert-danger');
 		}
 	showAlertAndHide(ms_showAlert);
+	showAll();
 }
 
 function isRemove(oldName){
@@ -67,6 +64,7 @@ function isRemove(oldName){
 			isSuccess = true;
 		}
 	}
+	console.log(isSuccess);
 	return isSuccess;
 }
 
@@ -109,9 +107,12 @@ function isUpdate(oldName, key, value){
 function showAll() {
 	// 显示所有记录
 	var thead, tbody = '';
+	$('#showAll').find('tbody').html(tbody);　//清空残留表格数据
 	for(var item in localStorage){
+		// console.log(typeof localStorage[item]);
 		if (typeof localStorage[item] == 'string') {
 			var p = JSON.parse(localStorage[item]);
+			// alert(p.db == db)
 			if (p.db == db) {
 				if (typeof thead == 'undefined') {
 					thead = writeThead(p);
@@ -119,9 +120,15 @@ function showAll() {
 				}
 				tbody += writeTbody(p);
 				$('#showAll').find('tbody').html(tbody);
-			}
+			} 
 		}
 	}
+	if (typeof thead == 'undefined') {
+		if ($('.alert').css('display') == 'none') { //检测弹框是否冲突
+			writeAlert('数据库没有任何记录','alert-warning');
+		}
+		showAlertAndHide(ms_showAlert);
+	} 
 }
 
 function writeThead(p) {
@@ -160,9 +167,9 @@ function newAndSave(){
 	var success = save(get());
 		$('#newRecordModal').modal('hide');
 		if (success) {
-			writeAlert("保存成功！", 'alert-success');
+			writeAlert("新建成功！", 'alert-success');
 		} else {
-			writeAlert("保存失败，姓名已存在！", 'alert-danger');
+			writeAlert("新建失败，姓名已存在！", 'alert-danger');
 		}
 		showAlertAndHide(ms_showAlert);
 		showAll();
